568fac1adbbb6d98cd79fbf1e634a8d20e1df08d
Matt <matt@mattwilks.ca>
2023-05-18 15:54:41 -0400

    Create widget.js

---
+++ widget.js
@@ -0,0 +1,11 @@
+<!-- Start of ChatBot (www.chatbot.com) code -->
+<script type="text/javascript">
+    window.__be = window.__be || {};
+    window.__be.id = "your-widget-id-goes-here";
+    (function() {
+        var be = document.createElement('script'); be.type = 'text/javascript'; be.async = true;
+        be.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.chatbot.com/widget/plugin.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(be, s);
+    })();
+</script>
+<!-- End of ChatBot code -->
